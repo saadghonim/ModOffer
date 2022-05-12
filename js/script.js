b80b43156f92a50cb75a3561064aca9a6add711d
saadghonim <86053620+saadghonim@users.noreply.github.com>
2022-05-12 17:13:18 +0200

    upload file

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ const click_user = document.querySelector(".click_user");
 const login_form = document.querySelector(".login_form");
 const close_log = document.querySelector(".close_log");
 
-console.log(close_log);
 
 $(document).ready(function(){
 //start slider
@@ -99,79 +98,79 @@ if ($(window).width() <= 767){
 });
 
 // menu & overlay
-open_menu.addEventListener("click", function(){
-  navBar_.classList.add("open_");
-  body_.classList.add("over_");
-  overlay.style.display = "block";
+// open_menu.addEventListener("click", function(){
+//   navBar_.classList.add("open_");
+//   body_.classList.add("over_");
+//   overlay.style.display = "block";
 
-});
-overlay.addEventListener("click", function(){
-  navBar_.classList.remove("open_");
-  body_.classList.remove("over_");
-  overlay.style.display = "none";
-  login_form.classList.remove("open_login");
+// });
+// overlay.addEventListener("click", function(){
+//   navBar_.classList.remove("open_");
+//   body_.classList.remove("over_");
+//   overlay.style.display = "none";
+//   login_form.classList.remove("open_login");
 
 
 
-});
-close_.addEventListener("click", function(){
-  navBar_.classList.remove("open_");
-  body_.classList.remove("over_");
-  overlay.style.display = "none";
+// });
+// close_.addEventListener("click", function(){
+//   navBar_.classList.remove("open_");
+//   body_.classList.remove("over_");
+//   overlay.style.display = "none";
 
-});
+// });
 
 // back_top
-var btn = $('.back_top');
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 100) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '500');
-});
-$(window).on('scroll', function() {
-  if($(window).scrollTop() > 300) {
-        $("nav").addClass("fixed");
-  } else {
-      $("nav").removeClass("fixed");
-  };
-});
+// var btn = $('.back_top');
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > 100) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:0}, '500');
+// });
+// $(window).on('scroll', function() {
+//   if($(window).scrollTop() > 300) {
+//         $("nav").addClass("fixed");
+//   } else {
+//       $("nav").removeClass("fixed");
+//   };
+// });
 
 // login 
-click_user.addEventListener("click", function(){
-  body_.classList.add("over_");
-  overlay.style.display = "block";
-  login_form.classList.add("open_login");
-})
-close_log.addEventListener("click", function(){
-  body_.classList.remove("over_");
-  overlay.style.display = "none";
-  login_form.classList.remove("open_login");
-})
+// click_user.addEventListener("click", function(){
+//   body_.classList.add("over_");
+//   overlay.style.display = "block";
+//   login_form.classList.add("open_login");
+// })
+// close_log.addEventListener("click", function(){
+//   body_.classList.remove("over_");
+//   overlay.style.display = "none";
+//   login_form.classList.remove("open_login");
+// })
 
-var mins = document.querySelector(".minutes");
-var hours = document.querySelector(".hours");
-var days = document.querySelector(".days");
+// var mins = document.querySelector(".minutes");
+// var hours = document.querySelector(".hours");
+// var days = document.querySelector(".days");
 
-const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
+// const second = 1000,
+//       minute = second * 60,
+//       hour = minute * 60,
+//       day = hour * 24;
 
-let countDown = new Date('May 4, 2022 06:50:00').getTime(),
-    x = setInterval(function() {
+// let countDown = new Date('May 4, 2022 06:50:00').getTime(),
+//     x = setInterval(function() {
       
-      let now = new Date().getTime(),
-          distance = countDown - now;
-      days.innerText = Math.floor(distance / (day)),
-      hours.innerText = Math.floor((distance % (day)) / (hour)),
-      mins.innerText = Math.floor((distance % (hour)) / (minute))
-    }, second)
+//       let now = new Date().getTime(),
+//           distance = countDown - now;
+//       days.innerText = Math.floor(distance / (day)),
+//       hours.innerText = Math.floor((distance % (day)) / (hour)),
+//       mins.innerText = Math.floor((distance % (hour)) / (minute))
+//     }, second)
 
 
     // ~~~~~~~~~~~~~~preload~~~~~~~~~~~~~~
@@ -217,3 +216,16 @@ let countDown = new Date('May 4, 2022 06:50:00').getTime(),
     // {
     //   check_element(set_element);
     // }
+
+
+$(document).ready(function(){
+  $('.btn_collapse_').click(function(){
+    var toggle = $(this).next('.toggle_collapse');
+    $(toggle).slideToggle("slow").prev(".color_toggle").toggleClass("poen_co");
+    $(toggle).prev().children().children(".backg_toggle").toggleClass("poen_backg");
+    $(toggle).parent().prev(".color_toggle").toggleClass("poen_co"); 
+    $(this).children().children('.bar--horizontal').toggleClass("is-active");
+  });
+});
+    
+
